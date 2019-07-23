f8e2c685b5929928e442a48c7fced984fcd9b9f6
Max Modesto Wallin <max.modesto-wallin@inyett.se>
2019-07-23 15:15:07 +0200

    Added Product Controller and added alternative routing in WebApiConfig in a not so good way

--- WebApiTestProject1/App_Start/WebApiConfig.cs
+++ WebApiTestProject1/App_Start/WebApiConfig.cs
@@ -15,6 +15,12 @@ namespace WebApiTestProject1
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+                name: "ProductsApi",
+                routeTemplate: "api/legacy/{id}",
+                defaults: new { controller = "product", id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
---
+++ WebApiTestProject1/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiTestProject1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void CreateProduct([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
