256c793ee2afadc84f021cfaef51b4e8e626e5ac
Max Modesto Wallin <max.modesto-wallin@inyett.se>
2019-07-24 10:04:33 +0200

    Added OrdersController for working with route precedence

---
+++ WebApiTestProject1/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiTestProject1.Controllers
+{
+    [RoutePrefix("orders")]
+    public class OrdersController : ApiController
+    {
+        /*
+         * Web Api has several sets of rules to determine route order. 
+         * In this case orders/complete falls into GetOrderById as it matches the length, 
+         * when it should go to GetOrderWithStatus.
+         * The best solution is to set up routing in a way that makes conflicts not possible.
+         * If that's not possible, use the Order property,
+         * It defaults to 0 and the lower number comes first.
+        */
+        // GET: orders/5
+        [HttpGet, Route("{id:length(8)}", Order = 2)]
+        public string GetOrderById(string id)
+        {
+            return "order-" + id;
+        }
+
+        // GET: orders
+        [HttpGet, Route("{status:regex(^(?i)(new|complete|pending)$)}", Order = 1)]
+        public IEnumerable<string> GetOrderWithStatus()
+        {
+            return new string[] { "status1", "status2" };
+        }
+    }
+}
