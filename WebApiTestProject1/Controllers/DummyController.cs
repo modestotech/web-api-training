df60b1cee7cd3e3d077c2fdba3492d5e98e069db
Max Modesto Wallin <max.modesto-wallin@inyett.se>
2019-07-24 11:00:39 +0200

    Moved some code to DummyController, and added a custom model binder for string concatenation from url params

---
+++ WebApiTestProject1/Controllers/DummyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using WebApiTestProject1.Models;
+
+namespace WebApiTestProject1.Controllers
+{
+    [RoutePrefix("dummy")]
+    public class DummyController : ApiController
+    {
+        // GET: dummy/date/7/25/2019
+        [HttpGet, Route("date/{*dateInput:datetime}")]
+        // The asterisk tells WebApi to bind to the parameter using all of the remaining segments.
+        public IEnumerable<string> GetOrderWithDate(DateTime dateInput)
+        {
+            return new string[] { "dummy1-" + dateInput.ToShortDateString(), "dummy2-" + dateInput.ToShortDateString() };
+        }
+
+        // GET: segments/string/to/concatenate
+        [HttpGet, Route("segments/{*array:maxlength(256)}")]
+        // WebAPI doesn't know how to bind a string array, so it's necessary with a custom model binder.
+        public string[] GetSegments([ModelBinder(typeof(StringArrayWildcardBinder))] string[] array)
+        {
+            return array;
+        }
+    }
+}
---
+++ WebApiTestProject1/Models/StringArrayWildcardBinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace WebApiTestProject1.Models
+{
+    public class StringArrayWildcardBinder : IModelBinder
+    {
+        public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+        {
+            var key = bindingContext.ModelName;
+            var val = bindingContext.ValueProvider.GetValue(key);
+
+            if (val != null)
+            {
+                var s = val.AttemptedValue;
+
+                if (s != null)
+                {
+                    try
+                    {
+                        // Parse the elements on the forward slash
+                        var array = s.Split('/');
+                        bindingContext.Model = array;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
