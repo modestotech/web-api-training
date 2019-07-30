e10d0ca9d44c81d0955e931f30538a44ec6a1783
Max Modesto Wallin <max.modesto-wallin@inyett.se>
2019-07-30 14:56:27 +0200

    Setup JWT authentication filter as well as certification files

---
+++ SendTestJWT/Program.cs
@@ -0,0 +1,94 @@
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendTestJWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Service URL to call
+            string _url = "http://localhost:64827/auth";
+
+            // The tokes isser urn, and the audience urn I intend this token for
+            string _issuer = "http://my.tokenissuer.com";
+            string _audience = "http://localhost:64827";
+
+            // Load the private key certificate from the local file
+            string path = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            var directory = System.IO.Path.GetDirectoryName(path);
+            var certFile = (directory + "\\Certificate\\CourseCert.pfx").Substring(6);
+            var certificate = new X509Certificate2();
+            certificate.Import(certFile, "abc12345", X509KeyStorageFlags.PersistKeySet);
+
+            // Create a digital signing key from the secret key in the certificate
+            var signingCredentials = new X509SecurityKey(certificate);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, "TestUserName"),
+                new Claim(ClaimTypes.Email, "testuser@mycompany.com"),
+                new Claim("MyCustomClaim", "My special value")
+            };
+
+            // Define the token elements
+            var securityTokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Audience = _audience,
+                Issuer = _issuer,
+                Subject = new ClaimsIdentity(claims, "Bearer"),
+                SigningCredentials = new SigningCredentials(signingCredentials,
+                                        SecurityAlgorithms.RsaSha256Signature),
+                IssuedAt = DateTime.UtcNow,
+                NotBefore = DateTime.UtcNow.AddMinutes(-1),
+                Expires = DateTime.UtcNow.AddHours(12)
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateJwtSecurityToken(securityTokenDescriptor);
+            var tokenString = handler.WriteToken(token);
+
+            Console.WriteLine(tokenString);
+
+            // Make a REST call with the JWT string
+            var task = Task.Run(async () => await GetValuesAsync(_url, tokenString));
+            var values = task.Result;
+
+            foreach (var s in values)
+                Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+
+        static async Task<List<string>> GetValuesAsync(string path, string token)
+        {
+            List<string> values = new List<string>();
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri(path);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Bearer", token);
+
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            if (response.IsSuccessStatusCode)
+                values = await response.Content.ReadAsAsync<List<string>>();
+            else
+                values.Add(response.ToString());
+
+            return values;
+        }
+    }
+}
---
+++ WebApiTestProject1/Filters/JwtAuthenticationFilter.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace WebApiTestProject1.Filters
+{
+    public class JwtAuthenticationFilter : Attribute, IAuthenticationFilter
+    {
+
+        public const string SupportedTokenScheme = "Bearer";
+        public bool AllowMultiple { get { return false; } }
+
+        string _audience = "http://localhost:64827";
+        string _validIssuer = "http://my.tokenissuer.com";
+
+        private readonly X509SecurityKey _signingCredentials;
+
+        public bool SendChallenge { get; set; }
+
+        public JwtAuthenticationFilter()
+        {
+            var filePath = HttpContext.Current.Server.MapPath("~/App_Data/CourseCert.cer");
+            var certificate = new X509Certificate2(filePath);
+            _signingCredentials = new X509SecurityKey(certificate);
+        }
+
+        public async Task AuthenticateAsync(HttpAuthenticationContext context,
+            CancellationToken cancellationToken)
+        {
+            var authHeader = context.Request.Headers.Authorization;
+
+            if (authHeader == null)
+                return;
+
+            var tokenType = authHeader.Scheme;
+            if (!tokenType.Equals(SupportedTokenScheme))
+                return;
+
+            var credentials = authHeader.Parameter;
+            if (string.IsNullOrEmpty(credentials))
+            {
+                context.ErrorResult = new AuthentiationFailureResult("Missing credentials", context.Request);
+                return;
+            }
+
+            try
+            {
+                IPrincipal principal = await ValidateCredentialsAsync(credentials, cancellationToken);
+
+                if (principal == null)
+                {
+                    context.ErrorResult = new AuthentiationFailureResult("Invalid security token", context.Request);
+                }
+                else
+                {
+                    context.Principal = principal;
+                }
+            }
+            catch (Exception stex) when (stex is SecurityTokenInvalidLifetimeException ||
+                                            stex is SecurityTokenExpiredException ||
+                                            stex is SecurityTokenNoExpirationException ||
+                                            stex is SecurityTokenNotYetValidException)
+            {
+                context.ErrorResult = new AuthentiationFailureResult("Security token lifetime error", context.Request);
+            }
+            catch (Exception)
+            {
+                context.ErrorResult = new AuthentiationFailureResult("Invalid security token", context.Request);
+            }
+
+        }
+
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            if (SendChallenge)
+            {
+                context.Result = new AddChallengeOnUnauthorizedResult(
+                    new AuthenticationHeaderValue(SupportedTokenScheme),
+                    context.Result);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private async Task<IPrincipal> ValidateCredentialsAsync(string credentials, CancellationToken cancellationToken)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+
+            // Verify validity of token
+            var isValidJwt = jwtHandler.CanReadToken(credentials);
+            if (!isValidJwt)
+                return null;
+
+            // Time to validate the Jwt internals
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidAudiences = new[] { _audience },
+
+                ValidateIssuer = true,
+                ValidIssuers = new[] { _validIssuer },
+
+                RequireSignedTokens = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKeys = new[] { _signingCredentials },
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromSeconds(5), // Limit the lifetime padding
+
+                NameClaimType = ClaimTypes.NameIdentifier,
+                AuthenticationType = SupportedTokenScheme,
+            };
+
+            SecurityToken validatedToken = new JwtSecurityToken();
+            ClaimsPrincipal principal = jwtHandler.ValidateToken(credentials, validationParameters, out validatedToken);
+
+            // Locally generated claims that should be available to downstream code
+            ((ClaimsIdentity)principal.Identity).AddClaim(new Claim("urn:Issuer",
+                validatedToken.Issuer));
+            ((ClaimsIdentity)principal.Identity).AddClaim(new Claim("urn:TokenScheme",
+                SupportedTokenScheme));
+
+            // If any downstream code might want the original token string, maybe to 
+            // make downstream calls, store it in a standard claim name or the bootstrap
+            // context.
+            ((ClaimsIdentity)principal.Identity).BootstrapContext = credentials;
+
+            return await Task.FromResult(principal);
+        }
+
+        private Tuple<string, string> ParseBasicAuthCredential(string credential)
+        {
+            string password = null;
+
+            var subject = (Encoding.GetEncoding("iso-8859-1")
+                .GetString(Convert.FromBase64String(credential)));
+
+            if (string.IsNullOrEmpty(subject))
+                return new Tuple<string, string>(null, null);
+
+            if (subject.Contains(":"))
+            {
+                var index = subject.IndexOf(':');
+                password = subject.Substring(index + 1);
+                subject = subject.Substring(0, index);
+            }
+
+            return new Tuple<string, string>(subject, password);
+        }
+    }
+}
