6f81f318e83af221d44ae098a181656868b33a4b
Max Modesto Wallin <max.modesto-wallin@inyett.se>
2019-07-30 08:26:50 +0200

    Used the client cache control in the filters controller

--- WebApiTestProject1/Controllers/FiltersController.cs
+++ WebApiTestProject1/Controllers/FiltersController.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -13,8 +14,10 @@ namespace WebApiTestProject1.Controllers
     {
         [HttpGet, Route("")]
         [RouteTimerFilter("GettAllValues")] // Overrides the name
+        [ClientCacheControlFilter(ClientCacheControl.Private, 10)]
         public IEnumerable<string> Get()
         {
+            Trace.WriteLine(DateTime.Now.ToLongTimeString() + " Get called");
             return new string[] { "value1", "value2" };
         }
 
